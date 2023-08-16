7054eb35eaf95b26dd88b54b5d00f8875134a03b
agumanz <128185771+agumanz@users.noreply.github.com>
2023-08-16 19:42:02 +0200

    Initialize Ruta-login

---
+++ src/front/js/pages/home.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import Moviestar from "../../img/Moviestar.png";
+import "../../styles/home.css";
+import "../../styles/SignIn.css";
+import SignIn from './SignIn.js';
+
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+	);
+};
